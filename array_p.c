2bfe56ac8e577b9522d16d285bd8f47245529bb3
emryswei <57343871+emryswei@users.noreply.github.com>
2021-08-13 13:22:08 +0800

    Add files via upload

---
+++ array_p.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[] = {1,2,3,4,5};
+    int *p = a;
+
+    for(int i=0;i<5;i++){
+        printf("%d", a[i]);      // array notation with a
+    }
+    for(int i=0;i<5;i++){
+        printf("%d", p[i]);      // array notation with p
+    }
+
+    for(int i=0;i<5;i++)
+        printf("%d", *(p+i));    // pointer notation with p
+    for(int i=0;i<5;i++)
+        printf("%d", *(a+i));    // pointer notation with p
+    return 0;
+}
